bb0a3430ebe21764bc3fe3996c964a56124a4eb2
rajeev2631007 <rajeev2631007@gmail.com>
2023-10-25 11:16:13 +0530

    Create Create_VM_ResourcesExit.ps1
    Scenario 1: Resources Exist
    
    In this scenario, we assume that the subscription, resource group, virtual network, and subnet are already created. We will focus on creating a virtual machine with the existing resources.
    
    Concept: A virtual machine (VM) is a scalable and flexible computing solution in Azure that allows you to run Windows or Linux workloads.
    
    Explanation:
    
        The script defines variables for the subscription, resource group, region, VM name, OS image, VM size, virtual network, subnet, NSG, and availability set.
        Connect-AzAccount signs in to your Azure account.
        Set-AzContext sets the current Azure subscription.
        New-AzVM creates a virtual machine with the specified configuration.

---
+++ Create_VM_ResourcesExit.ps1
@@ -0,0 +1,29 @@
+# Define your variables
+$subscriptionName = "YourSubscriptionName"
+$resourceGroupName = "YourResourceGroupName"
+$region = "YourRegion"
+$vmName = "YourVMName"
+$osImage = "YourOSImage"
+$vmSize = "YourVMSize"
+$vnetName = "YourVirtualNetworkName"
+$subnetName = "YourSubnetName"
+$nsgName = "YourNSGName"
+$availabilitySetName = "YourAvailabilitySetName"
+
+# Sign in to your Azure account
+Connect-AzAccount
+
+# Set the current Azure subscription
+Set-AzContext -Subscription $subscriptionName
+
+# Create a virtual machine
+New-AzVM `
+    -ResourceGroupName $resourceGroupName `
+    -Name $vmName `
+    -Image $osImage `
+    -Size $vmSize `
+    -VirtualNetworkName $vnetName `
+    -SubnetName $subnetName `
+    -Location $region `
+    -NetworkSecurityGroupName $nsgName `
+    -AvailabilitySet $availabilitySetName
